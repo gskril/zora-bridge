aa46c674dadba3758dd3db60b3a68481efed7b93
Greg <gskril@gmail.com>
2023-05-27 23:53:55 -0400

    add wallet connect

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 import './App.css'
 
 function App() {
-  return <></>
+  return (
+    <>
+      <ConnectButton />
+    </>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { WagmiConfig } from 'wagmi'
+import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
+import '@rainbow-me/rainbowkit/styles.css'
+
+import { chains, wagmiConfig } from './providers.ts'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains} modalSize="compact">
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  </React.StrictMode>
 )
